cfb4fac284fae2690bf3467bc2819c29576c709c
Victor Giers <mail@victorgiers.com>
2026-01-08 01:46:41 +0100

    Update EntryCard component with new styling and link formatting

--- client/src/components/EntryCard.tsx
+++ client/src/components/EntryCard.tsx
@@ -11,13 +11,15 @@ export default function EntryCard({ entry }: Props) {
 
   return (
     <div className="entry-card">
-      <div className="entry-card__title">{entry.title}</div>
-      <div className="entry-card__actions">
-        <Link className="button button--ghost" to={learnLink}>
-          Learn
+      <div className="entry-card__content">
+        <div className="entry-card__title">{entry.title}</div>
+      </div>
+      <div className="entry-card__actions inline">
+        <Link className="entry-link" to={learnLink}>
+          Learn &raquo;
         </Link>
-        <Link className="button" to={quizLink}>
-          Quiz
+        <Link className="entry-link" to={quizLink}>
+          Quiz &raquo;
         </Link>
       </div>
     </div>
